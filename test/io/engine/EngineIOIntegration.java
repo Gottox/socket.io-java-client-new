a8dffd7c90c5ab77b3941748e18ddcc6d2fae970
tox <devnull@localhost>
2012-04-30 10:22:12 +0200

    remove warnings

--- test/io/engine/EngineIOIntegration.java
+++ test/io/engine/EngineIOIntegration.java
@@ -3,18 +3,13 @@ package io.engine;
 import static org.junit.Assert.*;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.TimeUnit;
 
 import org.junit.After;
-import org.junit.AfterClass;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class EngineIOIntegration extends EngineIO {
